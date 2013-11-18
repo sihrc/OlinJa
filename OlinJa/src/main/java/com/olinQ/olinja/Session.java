c60b74fa0a8920e7a9b69c1d100de80f1256c228
Your Name <you@example.com>
2013-11-17 19:20:51 -0500

    added functions for listhashset

--- OlinJa/src/main/java/com/olinQ/olinja/Session.java
+++ OlinJa/src/main/java/com/olinQ/olinja/Session.java
@@ -9,9 +9,7 @@ import java.util.HashMap;
  */
 public class Session {
     String assignment,ninja,place,time,duration, date, id;
-    ArrayList<String> check = new ArrayList<String>();
-    ArrayList<String> checked = new ArrayList<String>();;
-    ArrayList<String> help = new ArrayList<String>();;
+    LinkedHashSet<String> check, checked, help;
 
     //public Constructor
     public Session(String assignment, String ninja, String place, String date, String time, String duration){
@@ -21,6 +19,9 @@ public class Session {
         this.date = date;
         this.time = time;
         this.duration = duration;
+        this.check = new LinkedHashSet<String>();
+        this.checked = new LinkedHashSet<String>();
+        this.help = new LinkedHashSet<String>();
     }
 
     //Public Methods
@@ -45,15 +46,62 @@ public class Session {
     public void setId(String value){
         this.id = value;
     }
-    public void setCheck(ArrayList<String> values){
+    public void setCheck(LinkedHashSet<String> values){
         this.check = values;
     }
-    public void setChecked(ArrayList<String> values){
+    public void setChecked(LinkedHashSet<String> values){
         this.checked = values;
     }
-    public void setHelp(ArrayList<String> values){
+    public void setHelp(LinkedHashSet<String> values){
         this.help = values;
     }
+
+
+    //Functions for changing states of students
+    public void addToCheck(String value){
+        this.check.add(value);
+    }
+    public void addToChecked(String value){
+        this.checked.add(value);
+    }
+    public void addToHelp(String value){
+        this.help.add(value);
+    }
+    public void removeFromCheck(String value){
+        this.check.remove(value);
+    }
+    public void removeFromChecked(String value){
+        this.checked.remove(value);
+    }
+    public void removeFromHelp(String value){
+        this.help.remove(value);
+    }
+
+    public void checkToChecked(String value){
+        addToChecked(value);
+        removeFromCheck(value);
+    }
+    public void checkToHelp(String value){
+        addToHelp(value);
+        removeFromCheck(value);
+    }
+    public void checkedToCheck(String value){
+        addToCheck(value);
+        removeFromChecked(value);
+    }
+    public void checkedToHelp(String value){
+        addToHelp(value);
+        removeFromChecked(value);
+    }
+    public void helpToCheck(String value){
+        addToCheck(value);
+        removeFromHelp(value);
+    }
+    public void helpToChecked(String value){
+        addToCheckec(value);
+        removeFromHelp(value);
+    }
+
 
     public FirebaseSession toFireBaseSession(){
         return new FirebaseSession(this.assignment, this.ninja, this.place, this.time, this.duration, this.date, this.check.toString(), this.help.toString(), this.checked.toString());
