068ed463a0ac073178a79dfd90150a99582a5ada
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-18 12:30:13 -0500

    Added hour display range in duration picker

--- OlinJa/src/main/java/com/olinQ/olinja/DurationPickerDialog.java
+++ OlinJa/src/main/java/com/olinQ/olinja/DurationPickerDialog.java
@@ -16,10 +16,14 @@ import java.util.List;
  */
 public class DurationPickerDialog extends TimePickerDialog
 {
+    //Listener and picker
     final OnTimeSetListener mCallback;
     TimePicker mTimePicker;
+
+    //Specified increment
     final int increment;
 
+    //Public constructor
     public DurationPickerDialog(Context context, OnTimeSetListener callBack, int hourOfDay, int minute, boolean is24HourView, int increment)
     {
         super(context, callBack, hourOfDay, minute/increment, true);
@@ -46,22 +50,41 @@ public class DurationPickerDialog extends TimePickerDialog
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        //Override the actual display for minutes
         try
         {
+            //Grab Specific TimePicker class
             Class<?> rClass = Class.forName("com.android.internal.R$id");
             Field timePicker = rClass.getField("timePicker");
             this.mTimePicker = (TimePicker)findViewById(timePicker.getInt(null));
-            Field m = rClass.getField("minute");
 
+            //Grab fields
+            Field m = rClass.getField("minute");
+            Field h = rClass.getField("hour");
+
+            //Creating a new number spinner for minutes
             NumberPicker mMinuteSpinner = (NumberPicker)mTimePicker.findViewById(m.getInt(null));
             mMinuteSpinner.setMinValue(0);
             mMinuteSpinner.setMaxValue((60/increment)-1);
             List<String> displayedValues = new ArrayList<String>();
+            //Add values to minutes
             for(int i=0;i<60;i+=increment)
             {
                 displayedValues.add(String.format("%02d", i));
             }
             mMinuteSpinner.setDisplayedValues(displayedValues.toArray(new String[0]));
+
+            //Creating a new number spinner for hours
+            NumberPicker mHourSpinner = (NumberPicker)mTimePicker.findViewById(h.getInt(null));
+            mMinuteSpinner.setMinValue(0); //Min Value
+            mMinuteSpinner.setMaxValue(5); //Max Value
+            List<String> hourValues = new ArrayList<String>();
+            //Add values to minutes
+            for (int i = 0; i < 5; i++) //Iterate through values wanted
+            {
+                hourValues.add(String.format("%d", i)); //Set Values
+            }
+            mHourSpinner.setDisplayedValues(hourValues.toArray(new String[0]));
         }
         catch (Exception e)
         {
