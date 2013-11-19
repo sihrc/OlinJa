8793f488c241acf9b5ecbef5e7948849de2b7df3
Chris Lee <sihrc.c.lee@gmail.com>
2013-11-18 21:23:19 -0500

    Added checked off activity

---
+++ OlinJa/src/main/java/com/olinQ/olinja/CheckedOffActivity.java
@@ -0,0 +1,80 @@
+package com.olinQ.olinja;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+/**
+ * Created by chris on 11/18/13.
+ */
+public class CheckedOffActivity extends Activity {
+    //ListView and Adapters
+    ListView checkedoffList;
+    QListAdapter checkedoffAdapter;
+
+    //Connectivity
+    ValueEventListener connected;
+
+    //Firebase URL Location
+    String FIREBASE_URL = "https://olinja-base.firebaseio.com";
+    Firebase checkedRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checked_off);
+
+        //Grab id
+        Intent in = getIntent();
+        String id = in.getStringExtra("id");
+
+        //Setup Firebase Reference
+        checkedRef = new Firebase(FIREBASE_URL + "/" + id);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        //Setup ListView
+        checkedoffList = (ListView) findViewById(R.id.checked_off_list);
+
+        //Setup list adapter
+        checkedoffAdapter = new QListAdapter(checkedRef,this, R.layout.q_list_item);
+
+        //Set List Adapter
+        checkedoffList.setAdapter(checkedoffAdapter);
+
+
+        //Connectivity Check
+        connected = checkedRef.getRoot().child(".info/connected").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean connected = (Boolean)dataSnapshot.getValue();
+                if (connected)
+                    Toast.makeText(CheckedOffActivity.this, "Connected to Assignment!", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(CheckedOffActivity.this, "Oh no! I can't find the internet!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {}
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.checked, menu);
+        return true;
+    }
+}
--- OlinJa/src/main/java/com/olinQ/olinja/SessionActivity.java
+++ OlinJa/src/main/java/com/olinQ/olinja/SessionActivity.java
@@ -29,6 +29,7 @@ public class SessionActivity extends Activity {
 
     //Add to queue Buttons
     TextView checkAdd, helpAdd;
+
     //Username
     String username;
 
@@ -122,7 +123,7 @@ public class SessionActivity extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.session, menu);
         return true;
     }
 
